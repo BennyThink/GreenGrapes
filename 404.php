b8b63656eca82225dd7fc82a719d71da976304ff
池程 (Takumi Ikezaki) <cydia2020@live.cn>
2017-05-25 14:24:32 +1000

    Add ja and en translation

--- 404.php
+++ 404.php
@@ -19,17 +19,17 @@
 <?php
 if(empty(Helper::options()->notFoundGame))
 {
-echo '<p>'._e('哎呦卧槽~404了！搜索试试？要不画画吧！').'</p>';
+echo '<p>'._e('哎呦卧槽~404了！搜索试试？要不画画吧！<br>残念、ページを見つかりませんでした。エラーコード４０４。検索してください！<br>Whoops, cannot find the page you are looking for. Please try search.').'</p>';
 echo '<br><embed width="560" height="270" src="https://okwbu9s8e.qnssl.com/canvas-drawing/html5-canvas-drawing-app.html" />';
 }	
 elseif (Helper::options()->notFoundGame==1)
 {
-echo '<p>'._e('哎呦卧槽~404了！搜索试试？或者玩玩我们的小恐龙？').'</p>';
+echo '<p>'._e('哎呦卧槽~404了！搜索试试？或者玩玩我们的小恐龙？<br>残念、ページを見つかりませんでした。エラーコード４０４。ゲームを遊びましょうか？<br>Whoops, cannot find the page you are looking for. Maybe some light gaming?').'</p>';
 echo '<br><embed width="330" height="280" src="https://okwbu9s8e.qnssl.com/t-rex-runner/index.html" />';
 }	
 elseif (Helper::options()->notFoundGame==2)
 	{
-echo '<p>'._e('哎呦卧槽~404了！搜索试试？要不帮我喂喂可怜的小松鼠吧').'</p>';
+echo '<p>'._e('哎呦卧槽~404了！搜索试试？要不帮我喂喂可怜的小松鼠吧<br>残念、ページを見つかりませんでした。エラーコード４０４。リスをてたすけしてくださいね。<br>Whoops, cannot find the page you are looking for. Maybe help my squirrel.').'</p>';
 echo '<br><embed width="290" height="230" src="https://o51bfbumd.qnssl.com/h5hamster.html" />';
 }	
 ?>
